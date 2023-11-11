084404898fd894a652e2942b00acee348a5d4830
Youssif.Aziz <youssif.aziz@cyshield.com>
2023-11-11 02:03:52 +0200

    add options in notifier

--- go-notifier.go
+++ go-notifier.go
@@ -4,9 +4,13 @@ import (
 	"github.com/joaziz/go-notifier/external"
 	"github.com/joaziz/go-notifier/external/drivers"
 	"github.com/joaziz/go-notifier/internal"
+	"log/slog"
 )
 
 func NewInterNotifier(name string, opt internal.Options) *internal.Notifier {
+	if opt.Logger == nil {
+		opt.Logger = slog.Default()
+	}
 	return internal.New(name, opt)
 }
 
