9c558ca88ecf7304a4df100e825d3e35e9f56c3e
Youssif.Aziz <youssif.aziz@cyshield.com>
2023-11-30 02:35:50 +0200

    change interior package

--- go-notifier.go
+++ go-notifier.go
@@ -3,15 +3,15 @@ package go_notifier
 import (
 	"github.com/joaziz/go-notifier/external"
 	"github.com/joaziz/go-notifier/external/drivers"
-	"github.com/joaziz/go-notifier/internal"
+	"github.com/joaziz/go-notifier/local"
 	"log/slog"
 )
 
-func NewInterNotifier(name string, opt internal.Options) *internal.Notifier {
+func NewInterNotifier(name string, opt local.Options) *local.Notifier {
 	if opt.Logger == nil {
 		opt.Logger = slog.Default()
 	}
-	return internal.New(name, opt)
+	return local.New(name, opt)
 }
 
 func NewExternalNotifier(driver drivers.DriverInterface) *external.Notifier {
--- local/options.go
+++ local/options.go
@@ -1,4 +1,4 @@
-package internal
+package local
 
 import "log/slog"
 
